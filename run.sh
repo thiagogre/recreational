212440c128f8b3e154967ba27b1d08476d601c0b
thiagogre <thiagogre.dev@gmail.com>
2025-08-17 10:42:13 -0300

    Apply the flow for all files

--- run.sh
+++ run.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-FILE=$1
+for FILE in test_*.c; do
+    [ -e "$FILE" ] || continue  # skip if no matches
 
-clang-format -i $FILE
-gcc $FILE -o output.o -Wall -Wextra -Werror -pedantic
-./output.o
+    clang-format -i "$FILE"
+
+    OUTPUT="${FILE%.c}.o"
+
+    echo "[COMPILE] $FILE -> $OUTPUT"
+    if gcc "$FILE" -o "$OUTPUT" -Wall -Wextra -Werror -pedantic; then
+        ./"$OUTPUT"
+        echo
+    else
+        echo "[ERROR] $FILE"
+        echo
+    fi
+done
